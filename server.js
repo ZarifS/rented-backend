eb1f0fef295316a8e89343d70fedca218ea5b19e
Zarif Shahriar <zarif.shahriar@gmail.com>
2018-11-27 21:57:51 -0500

    Removed uneeded files and adding user creation

--- server.js
+++ server.js
@@ -2,16 +2,6 @@ const express = require("express");
 const firebase = require("firebase");
 const app = express();
 const port = process.env.PORT || 8000;
-app.get("/api/hello", (req, res) => {
-  res.send({ express: "Hello From Express" });
-});
-
-app.get("/api/addUser", (req, res) => {
-  writeUserData(req.query.userId, req.query.name, req.query.email).then(() => {
-    res.send({ express: "Added User!" });
-  });
-});
-
 var config = {
   apiKey: "AIzaSyBag4e7WLKhjPvZn6Sjvy3Qf7tmse5cRdQ",
   authDomain: "rented-project.firebaseapp.com",
@@ -20,15 +10,20 @@ var config = {
   storageBucket: "rented-project.appspot.com",
   messagingSenderId: "426485648453"
 };
-
 firebase.initializeApp(config);
-const database = firebase.database();
+const auth = firebase.auth()
 
-async function writeUserData(userId, name, email) {
-  firebase
-    .database()
-    .ref("users/" + userId)
-    .set({ username: name, email: email });
-}
+app.get("/api/hello", (req, res) => {
+  res.send({express: "Hello From Express"});
+});
+
+app.get("/api/createUser", (req, res) => {
+  const promise = auth.createUserWithEmailAndPassword(req.query.email, req.query.password);
+  promise.then(user => {
+    res.send({user});
+  }).catch(e => {
+    res.send({error: e.message});
+  })
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
